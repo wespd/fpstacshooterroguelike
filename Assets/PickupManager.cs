7ec9822feefe7b769b6110ecf5d89bd52139a75d
wespd <37629858+wespd@users.noreply.github.com>
2023-09-07 18:52:52 -0700

    added item and basics for item manager

---
+++ Assets/PickupManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+    public Item currentItem;
+    public GameObject itemObject;
+
+    public float pickupRange;
+
+    public KeyCode pickupKey = KeyCode.E;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(pickupKey))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, pickupRange))
+            {
+                Item hitItem = hit.collider.GetComponent<Item>();
+                if(hitItem != null)
+                {
+                    currentItem = hitItem;
+                }
+            }
+        }
+    }
+
+    
+}
