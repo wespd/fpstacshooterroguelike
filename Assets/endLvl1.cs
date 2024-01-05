154c8230c65b9736643ee30e3d08be117179c934
Everythingcovered <107648413+Everythingcovered@users.noreply.github.com>
2024-01-05 11:47:37 -0800

    a

--- Assets/endLvl1.cs
+++ Assets/endLvl1.cs
@@ -6,8 +6,10 @@ public class endLvl1 : MonoBehaviour
 {
     public Transform playerTransform;
     public Transform level2Location;
+    public Rigidbody rB;
     public void moveLocation() 
     {
         playerTransform.position = level2Location.position;
+        rB.velocity = Vector3.zero;
     }
 }
