eac5581aa74771f416114ed274538972f7ab32f1
wespd <37629858+wespd@users.noreply.github.com>
2024-02-20 11:25:13 -0800

    tp switches

--- Assets/Interact/tpInteractable.cs
+++ Assets/Interact/tpInteractable.cs
@@ -6,6 +6,8 @@ public class tpInteractable : Interactable
 {
     public override void InteractTrigger(Interact other)
     {
+        Vector3 temp = other.gameObject.transform.position;
         other.gameObject.transform.position = transform.position;
+        transform.position = temp;
     }
 }
