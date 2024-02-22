d0157fb7b90b6ebc27a82f2e368242f0b94b5650
wespd <37629858+wespd@users.noreply.github.com>
2024-02-22 11:40:30 -0800

    started level design on the tylized scene

--- Assets/Interact/Interact.cs
+++ Assets/Interact/Interact.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Interact : MonoBehaviour
 {
-    float interactDistance;
+    public float interactDistance;
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit);
-            if(hit.collider != null)
+            if(hit.collider != null && hit.distance < interactDistance)
             {
                 Interactable hitObject = hit.collider.GetComponent<Interactable>();
                 if(hitObject != null && hit.distance >= interactDistance)
