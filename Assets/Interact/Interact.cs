a3af4ca1ca55d28bd41d9b5b0fdc1ed4d1fda6b3
wespd <37629858+wespd@users.noreply.github.com>
2024-02-26 11:38:38 -0800

    changed tp

--- Assets/Interact/Interact.cs
+++ Assets/Interact/Interact.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interact : MonoBehaviour
 {
     public float interactDistance;
+    public Image canInteractImage;
+    public Image cantInteractImage;
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit);
+        Interactable hitObject = hit.collider != null? hit.collider.GetComponent<Interactable>() : null;
+        bool canInteract = Vector3.Distance(hit.point, transform.position) <= interactDistance && hitObject != null;
+        if(canInteract)
         {
-            RaycastHit hit;
-            Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit);
-            if(hit.collider != null && hit.distance < interactDistance)
-            {
-                Interactable hitObject = hit.collider.GetComponent<Interactable>();
-                if(hitObject != null && hit.distance >= interactDistance)
-                {
-                    hitObject.InteractTrigger(this);
-                }
-            }
+            canInteractImage.enabled = true;
+            cantInteractImage.enabled = false;
+        }
+        else
+        {
+            cantInteractImage.enabled = true;
+            canInteractImage.enabled = false;
+        }
+        if(Input.GetMouseButtonDown(0) && canInteract)
+        {   
+            hitObject.InteractTrigger(this);
         }
         
     }
